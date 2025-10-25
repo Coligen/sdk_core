9a49ea03c475c871ab71cb8824350895ab814ba8
River <200612653@qq.com>
2025-10-25 15:51:30 +0800

    remove android platform implementation

--- android/src/main/java/mobile/open/plugin/sdk_core/SdkCorePlugin.java
+++ android/src/main/java/mobile/open/plugin/sdk_core/SdkCorePlugin.java
@@ -4,34 +4,22 @@ import androidx.annotation.NonNull;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
-import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import mobile.open.sdk.core.SDKCore;
 
 public class SdkCorePlugin implements FlutterPlugin, MethodCallHandler {
-  private MethodChannel channel;
-
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "mobile.open.sdk/core");
-    channel.setMethodCallHandler(this);
+
   }
 
   @Override
   public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-    if ("init".equals(call.method)) {
-      result.success(0);
-    } else if ("initializeOnUserProtocolAgreed".equals(call.method)) {
-      SDKCore.initializeOnUserProtocolAgreed();
-      result.success(0);
-    } else {
-      result.notImplemented();
-    }
+    
   }
 
   @Override
   public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-    channel.setMethodCallHandler(null);
+
   }
 }
